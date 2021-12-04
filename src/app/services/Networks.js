386c7ecc52401a0dfa3b9d6ef64ddb1066ef3616
Daniel Rodriguez <drg809@gmail.com>
2021-12-04 11:25:13 +0100

    cambios

--- src/app/services/Networks.js
+++ src/app/services/Networks.js
@@ -3,6 +3,7 @@ export const DevNetworks = [
     index: 0,
     image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png',
     name: 'Binance Smart Chain Testnet',
+    routerAddress: '0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3',
     params: {
       chainId: '0x61',
       chainName: 'Binance Smart Chain Testnet',
@@ -35,37 +36,13 @@ export const DevNetworks = [
         image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/825.png',
         address: "0x7ef95a0fee0dd31b22626fa2e10ee6a223f8a684"
       },
-      {
-        symbol: "CAKE",
-        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/7186.png',
-        address: "0xdsfsd"
-      },
-      {
-        symbol: "SOLANA",
-        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/5426.png',
-        address: "0xdsfsd"
-      },
-      {
-        symbol: "MATIC",
-        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/3890.png',
-        address: "0xdsfsd"
-      },
-      {
-        symbol: "ETHER",
-        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png',
-        address: "0xdsfsd"
-      },
-      {
-        symbol: "HARMONY",
-        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/3945.png',
-        address: "0xdsfsd"
-      },
     ]
   },
   {
     index: 1,
     name: 'Ethereum',
     image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png',
+    routerAddress: '0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3',
     params: {
       chainId: '0x1',
       chainName: 'Ethereum',
@@ -90,7 +67,7 @@ export const DevNetworks = [
       },
       {
         symbol: "USDC",
-        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/4687.png',
+        image: 'https://bscscan.com/token/images/centre-usdc_28.png',
         address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
       },
       {
@@ -119,6 +96,7 @@ export const DevNetworks = [
     index: 2,
     name: 'Polygon (MATIC)',
     image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/3890.png',
+    routerAddress: '0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3',
     params: {
       chainId: '0x89',
       chainName: 'Polygon (MATIC)',
@@ -167,6 +145,7 @@ export const DevNetworks = [
     index: 3,
     name: "Harmony Mainnet Shard 0",
     image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/3945.png',
+    routerAddress: '0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3',
     params: {
       chainId: '0x63564C40',
       chainName: 'Harmony',
@@ -196,7 +175,7 @@ export const DevNetworks = [
       },
       {
         symbol: "USDC",
-        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png',
+        image: 'https://bscscan.com/token/images/centre-usdc_28.png',
         address: "one1np293efrmv74xyjcz0kk3sn53x0fm745f2hsuc"
       },
       {
@@ -215,6 +194,7 @@ export const DevNetworks = [
     index: 4,
     name: "Fantom Opera",
     image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/3513.png',
+    routerAddress: '0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3',
     params: {
       chainId: '0xFA',
       chainName: 'Fantom',
@@ -239,7 +219,7 @@ export const DevNetworks = [
       },
       {
         symbol: "USDC",
-        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png',
+        image: 'https://bscscan.com/token/images/centre-usdc_28.png',
         address: "0x04068da6c83afcfa0e13ba15a6696662335d5b75"
       },
       {
@@ -258,6 +238,7 @@ export const DevNetworks = [
     index: 5,
     name: "RSK Testnet",
     image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/3626.png',
+    routerAddress: '0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3',
     params: {
       chainId: '0x1F',
       chainName: 'RSK',
@@ -301,6 +282,7 @@ export const DevNetworks = [
     index: 5,
     name: "OKExChain Testnet",
     image: 'https://s2.coinmarketcap.com/static/img/exchanges/64x64/294.png',
+    routerAddress: '0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3',
     params: {
       chainId: '0x41',
       chainName: 'OKT',
@@ -344,6 +326,7 @@ export const DevNetworks = [
     index: 6,
     name: "IoTeX Network Testnet",
     image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/2777.png',
+    routerAddress: '0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3',
     params: {
       chainId: '0x1252',
       chainName: 'iotex.io',
@@ -390,6 +373,7 @@ export const ProdNetworks = [
     index: 0,
     image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png',
     name: 'Binance Smart Chain',
+    routerAddress: '0x10ED43C718714eb63d5aA57B78B54704E256024E',
     params: {
       chainId: '0x38',
       chainName: 'Binance Smart Chain Mainnet',
@@ -405,27 +389,42 @@ export const ProdNetworks = [
       {
         symbol: "BNB",
         image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png',
+        address: "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"
+      },
+      {
+        symbol: "BUSD",
+        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/4687.png',
+        address: "0xe9e7cea3dedca5984780bafc599bd69add087d56"
+      },
+      {
+        symbol: "DAI",
+        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/4943.png',
+        address: "0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3"
+      },
+      {
+        symbol: "USDT",
+        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/825.png',
+        address: "0x55d398326f99059ff775485246999027b3197955"
+      },
+      {
+        symbol: "CAKE",
+        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/7186.png',
+        address: "0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82"
+      },
+      {
+        symbol: "SOLANA",
+        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/5426.png',
         address: "0xdsfsd"
       },
       {
-        symbol: "BNB",
-        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png',
-        address: "0xdsfsd"
+        symbol: "MATIC",
+        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/3890.png',
+        address: "0xcc42724c6683b7e57334c4e856f4c9965ed682bd"
       },
       {
-        symbol: "BNB",
-        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png',
-        address: "0xdsfsd"
-      },
-      {
-        symbol: "BNB",
-        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png',
-        address: "0xdsfsd"
-      },
-      {
-        symbol: "BNB",
-        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png',
-        address: "0xdsfsd"
+        symbol: "ETHER",
+        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png',
+        address: "0x2170ed0880ac9a755fd29b2688956bd959f933f8"
       },
     ]
   },
@@ -433,6 +432,7 @@ export const ProdNetworks = [
     index: 1,
     name: 'Ethereum',
     image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png',
+    routerAddress: '0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3',
     params: {
       chainId: '0x1',
       chainName: 'Ethereum',
@@ -446,29 +446,39 @@ export const ProdNetworks = [
     },
     acceptedPaymentTokens: [
       {
-        symbol: "BNB",
-        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png',
-        address: "0xdsfsd"
+        symbol: "ETHER",
+        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png',
+        address: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
       },
       {
         symbol: "BNB",
         image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png',
-        address: "0xdsfsd"
+        address: "0xB8c77482e45F1F44dE1745F52C74426C631bDD52"
       },
       {
-        symbol: "BNB",
-        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png',
-        address: "0xdsfsd"
+        symbol: "USDC",
+        image: 'https://bscscan.com/token/images/centre-usdc_28.png',
+        address: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
       },
       {
-        symbol: "BNB",
-        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png',
-        address: "0xdsfsd"
+        symbol: "DAI",
+        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/4943.png',
+        address: "0x6b175474e89094c44da98b954eedeac495271d0f"
       },
       {
-        symbol: "BNB",
-        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png',
-        address: "0xdsfsd"
+        symbol: "USDT",
+        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/825.png',
+        address: "0xdac17f958d2ee523a2206206994597c13d831ec7"
+      },
+      {
+        symbol: "MATIC",
+        image: 'https://etherscan.io/token/images/matic-polygon_32.png',
+        address: "0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0"
+      },
+      {
+        symbol: "HARMONY",
+        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/3945.png',
+        address: "0x799a4202c12ca952cb311598a024c80ed371a41e"
       },
     ]
   },
@@ -476,6 +486,7 @@ export const ProdNetworks = [
     index: 2,
     name: 'Polygon (MATIC)',
     image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/3890.png',
+    routerAddress: '0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff',
     params: {
       chainId: '0x89',
       chainName: 'Polygon (MATIC)',
@@ -489,30 +500,128 @@ export const ProdNetworks = [
     },
     acceptedPaymentTokens: [
       {
-        symbol: "BNB",
-        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png',
-        address: "0xdsfsd"
+        symbol: "MATIC",
+        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/3890.png',
+        address: "0x0000000000000000000000000000000000001010"
       },
       {
         symbol: "BNB",
         image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png',
-        address: "0xdsfsd"
+        address: "0x3BA4c387f786bFEE076A58914F5Bd38d668B42c3"
+      },
+      {
+        symbol: "BUSD",
+        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/4687.png',
+        address: "0xdab529f40e671a1d4bf91361c21bf9f0c9712ab7"
+      },
+      {
+        symbol: "DAI",
+        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/4943.png',
+        address: "0x8f3cf7ad23cd3cadbd9735aff958023239c6a063"
+      },
+      {
+        symbol: "USDT",
+        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/825.png',
+        address: "0xc2132d05d31c914a87c6611c10748aeb04b58e8f"
+      },
+      {
+        symbol: "ETHER",
+        image: 'https://polygonscan.com/token/images/wETH_32.png',
+        address: "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619"
+      },
+    ]
+  },
+  {
+    index: 3,
+    name: "Harmony Mainnet Shard 0",
+    image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/3945.png',
+    routerAddress: '0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506',
+    params: {
+      chainId: '0x63564C40',
+      chainName: 'Harmony',
+      nativeCurrency: {
+        name: 'ONE',
+        symbol: 'ONE',
+        decimals: 18
+      },
+      rpcUrls: ['https://api.harmony.one'],
+      blockExplorerUrls: ['https://explorer.harmony.one']
+    },
+    acceptedPaymentTokens: [
+      {
+        symbol: "ONE",
+        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/3945.png',
+        address: "0xcf664087a5bb0237a0bad6742852ec6c8d69a27a"
       },
       {
         symbol: "BNB",
         image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png',
-        address: "0xdsfsd"
+        address: "0xb1f6e61e1e113625593a22fa6aa94f8052bc39e0"
+      },
+      {
+        symbol: "DAI",
+        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/4943.png',
+        address: "0xef977d2f931c1978db5f6747666fa1eacb0d0339"
+      },
+      {
+        symbol: "USDC",
+        image: 'https://bscscan.com/token/images/centre-usdc_28.png',
+        address: "0x985458e523db3d53125813ed68c274899e9dfab4"
+      },
+      {
+        symbol: "BUSD",
+        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/4687.png',
+        address: "0xe176ebe47d621b984a73036b9da5d834411ef734"
+      },
+      {
+        symbol: "USDT",
+        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/825.png',
+        address: "0x3c2b8be99c50593081eaa2a724f0b8285f5aba8f"
+      },
+    ]
+  },
+  {
+    index: 4,
+    name: "Avalanche Mainnet",
+    image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/3945.png',
+    routerAddress: '0x262DcFB36766C88E6A7a2953c16F8defc40c378A',
+    params: {
+      chainId: '0x0000A86A',
+      chainName: 'AVAX',
+      nativeCurrency: {
+        name: 'Avalanche',
+        symbol: 'AVAX',
+        decimals: 18
+      },
+      rpcUrls: ['https://api.avax.network/ext/bc/C/rpc'],
+      blockExplorerUrls: ['https://snowtrace.io/']
+    },
+    acceptedPaymentTokens: [
+      {
+        symbol: "AVAX",
+        image: 'https://snowtrace.io/token/images/avax_32.png',
+        address: "0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7"
+      },
+      {
+        symbol: "USDT",
+        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/825.png',
+        address: "0xc7198437980c041c805a1edcba50c1ce5db95118"
       },
       {
         symbol: "BNB",
         image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png',
-        address: "0xdsfsd"
+        address: "0x264c1383ea520f73dd837f915ef3a732e204a493"
       },
       {
-        symbol: "BNB",
-        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/1839.png',
-        address: "0xdsfsd"
+        symbol: "DAI",
+        image: 'https://s2.coinmarketcap.com/static/img/coins/64x64/4943.png',
+        address: "0xd586e7f844cea2f87f50152665bcbc2c279d8d70"
       },
+      {
+        symbol: "USDC",
+        image: 'https://bscscan.com/token/images/centre-usdc_28.png',
+        address: "0xa7d7079b0fead91f3e65f86e8915cb59c1a4c664"
+      }
     ]
   },
 ];
