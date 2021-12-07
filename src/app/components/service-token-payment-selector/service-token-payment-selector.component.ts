6f84c7415cf4936456afb4645f6be4ff29d29a74
Paulo <pauloxti@gmail.com>
2021-12-07 15:59:57 +0100

    network id from web3 service

--- src/app/components/service-token-payment-selector/service-token-payment-selector.component.ts
+++ src/app/components/service-token-payment-selector/service-token-payment-selector.component.ts
@@ -12,21 +12,21 @@ export class ServiceTokenPaymentSelectorComponent implements OnInit {
   networks: any = DevNetworks;
   currentToken = 0;
 
-  @Input() networkId = 0;
-  
+
   @Output() selectedTokenAddres = new EventEmitter();
-  acceptedPaymentTokens = this.networks[this.networkId].acceptedPaymentTokens;
+  acceptedPaymentTokens = this.networks[0].acceptedPaymentTokens;
+
   constructor(public web3Service: Web3Service) {
 
-    //algunas veces manda networkId como un numero y otras como un hexadecimal como texto, funcionan la bsc y eter
-    //porque coincide en ambos pero al cambiar a polygon o harmony peta. Lo he intentado convirtiendo a hex primero 
-    //con this.networdId.toFixed(16) y luego con este código de abajo. Sigue petando a veces, hay que unificar todo,
-    //no podemos mandar un number y en network.js tener hexadecimales.
+    // algunas veces manda networkId como un numero y otras como un hexadecimal como texto, funcionan la bsc y eter
+    // porque coincide en ambos pero al cambiar a polygon o harmony peta. Lo he intentado convirtiendo a hex primero
+    // con this.networdId.toFixed(16) y luego con este código de abajo. Sigue petando a veces, hay que unificar todo,
+    // no podemos mandar un number y en network.js tener hexadecimales.
 
-    const res = this.networks.find(x => x.params.chainId == "0x"+("0"+(Number(this.networkId).toString(16))).slice(-2).toUpperCase());
-    if (res) this.networkId = res.index;
-    
-    this.acceptedPaymentTokens = this.networks[this.networkId].acceptedPaymentTokens;
+    const res = this.networks.find(x => x.params.chainId === '0x' + ('0' + (Number(this.web3Service.networkId).toString(16))).slice(-2).toUpperCase());
+    if (res) { this.web3Service.networkId = res.index; }
+
+    this.acceptedPaymentTokens = this.networks[this.web3Service.networkId].acceptedPaymentTokens;
 
     // this.acceptedPaymentTokens = this.networks[this.networkId].acceptedPaymentTokens;
     // console.log({a: this.networkId});
@@ -37,10 +37,11 @@ export class ServiceTokenPaymentSelectorComponent implements OnInit {
 
   // tslint:disable-next-line:typedef
   async OnPaymentTokenSelectChange(changeEvent: MatSelectChange) {
-    const res = this.networks.find(x => x.params.chainId == "0x"+("0"+(Number(this.networkId).toString(16))).slice(-2).toUpperCase());
-    if (res) this.networkId = res.index;
-    
-    console.log({i:this.networkId});
+    const res = this.networks.find(x => x.params.chainId === '0x' +
+      ('0' + (Number(this.web3Service.networkId).toString(16))).slice(-2).toUpperCase());
+    if (res) { this.web3Service.networkId = res.index; }
+
+    console.log({i: this.web3Service.networkId});
     // this.selectedTokenAddres.emit({...this.networks[this.networkId].acceptedPaymentTokens[changeEvent.value], id:changeEvent.value});
     // this.currentToken = changeEvent.value;
   }
