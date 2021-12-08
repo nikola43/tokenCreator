e75b6ad20e6d7ddb2e4161babe36f1550f840221
paulo <pauloxti@gmail.com>
2021-12-08 20:39:27 +0100

    last token abi

--- syncFront.sh
+++ syncFront.sh
@@ -2,3 +2,4 @@
 #rm -rf ./dist && yarn build && rsync -a dist/TokenGenerator/ root@178.62.74.110:/var/www/midastool.io/
 rm -rf ./dist && yarn build && rsync -a dist/TokenGenerator/ root@178.62.74.110:/var/www/app.midastool.io/
 #-ArdBoard23
+ 
